701d871fc23d490256d3e51376684f4f87bc24de
mDanone <daniyalbekow@icloud.com>
2021-02-26 15:16:57 +0300

    добавил exceptions

--- ads/scrapers.py
+++ ads/scrapers.py
@@ -16,36 +16,46 @@ def parser_hh(url):
     for i in range(41000000, max_ad_num):
         url = f'https://hh.ru/vacancy/{i}'
         if ads.filter(vacancy_url=url):
-            print("Объявление уже естьв базе")
+            print(f"{url} в базе")
             continue
         parser.driver.get(url)
+        # Есть страница в которых требуется авторизация, их мы пропускаем
         try:
-            parser.driver.find_element_by_class_name("attention_bad")
-            continue
+            attention = parser.driver.find_element_by_class_name("attention_bad")
         except:
-            title = parser.get_title()
-            company_name = parser.get_company_name()
-            city = parser.get_address()
-            salary = parser.get_salary()
-            work_experience = parser.get_experience()
-            type_of_employment = parser.get_type_of_employment()
-            description = parser.get_description()
-            if title == '':
-                state = 'Не найдено'
-            elif title != '' and description == '':
-                state = 'В архиве'
-            r = requests.post("http://135.181.195.100:8888/api/v1/ads/", {'title': title,
-                                                                          'company_name': company_name,
-                                                                          'city': city,
-                                                                          'salary': salary,
-                                                                          'work_experience': work_experience,
-                                                                          'employment_type': type_of_employment,
-                                                                          'description': description,
-                                                                          'vacancy_url': url,
-                                                                          'phone': '',
-                                                                          'email': '',
-                                                                          'state': state})
-            print(r.status_code)
+            attention = ''
+        if attention:
+            continue
+        # Проверка на не найденную страницу
+        try:
+            not_found = parser.driver.find_element_by_class_name("error__content")
+        except:
+            not_found = ''
+        if not_found:
+            continue
+        title = parser.get_title()
+        company_name = parser.get_company_name()
+        city = parser.get_address()
+        salary = parser.get_salary()
+        work_experience = parser.get_experience()
+        type_of_employment = parser.get_type_of_employment()
+        description = parser.get_description()
+        if title == '':
+            state = 'Не найдено'
+        elif title != '' and description == '':
+            state = 'В архиве'
+        r = requests.post("http://135.181.195.100:8888/api/v1/ads/", {'title': title,
+                                                                        'company_name': company_name,
+                                                                        'city': city,
+                                                                        'salary': salary,
+                                                                        'work_experience': work_experience,
+                                                                        'employment_type': type_of_employment,
+                                                                        'description': description,
+                                                                        'vacancy_url': url,
+                                                                        'phone': '',
+                                                                        'email': '',
+                                                                        'state': state})
+        print(r.status_code)
     
 
 
